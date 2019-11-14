16cb49aaa6c51ccf398a0e9ced33364c6ca8a06c
starrytky <starrytky@qq.com>
2019-11-14 22:39:31 +0800

    监听点击事件、阻止冒泡

--- js/index.js
+++ js/index.js
@@ -1,60 +1,60 @@
 let keys = [
-    ['1','2','3','4','5','6','7','8','9','0'],
-    ['q','w','e','r','t','y','u','i','o','p'],
-    ['a','s','d','f','g','h','j','k','l'],
-    ['z','x','c','v','b','n','m']
+    ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
+    ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+    ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+    ['z', 'x', 'c', 'v', 'b', 'n', 'm']
 ];
 
 let sites = {
-    'w':'wangdoc.com',
-    "t":"translate.google.cn",
-    "b":"bitwiser.in/blurt",
-    "c":"tympanus.net/codrops",
-    "d":"dribbble.com",
-    "m":"developer.mozilla.org/zh-CN/docs/Web/JavaScript",
-    "g":"github.com",
-    "j":"jquery.com",
-    "n":"nodejs.org/zh-cn",
-    "v":"cn.vuejs.org",
-    "r":"zh-hans.reactjs.org",
-    "s":"www.mongodb.com/cn",
-    "z":"www.colorzilla.com",
-    "p":"compressjpeg.com",
-    "h":"https://wallhaven.cc/",
-    "x":"https://www.cssmatic.com/box-shadow"
+    'w': 'wangdoc.com',
+    "t": "translate.google.cn",
+    "b": "bitwiser.in/blurt",
+    "c": "tympanus.net/codrops",
+    "d": "dribbble.com",
+    "m": "developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+    "g": "github.com",
+    "j": "jquery.com",
+    "n": "nodejs.org/zh-cn",
+    "v": "cn.vuejs.org",
+    "r": "zh-hans.reactjs.org",
+    "s": "www.mongodb.com/cn",
+    "z": "www.colorzilla.com",
+    "p": "compressjpeg.com",
+    "h": "https://wallhaven.cc/",
+    "x": "https://www.cssmatic.com/box-shadow"
 };
 let siteImgs = {
-    "w":"http://wangdoc.com/assets/icons/favicon-32x32.png",
-    "t":"http://translate.google.cn/favicon.ico",
-    "b":"http://bitwiser.in/favicon.ico",
-    "c":"http://codropspz-tympanus.netdna-ssl.com/codrops/wp-content/themes/codropstheme03/favicons/favicon.ico",
-    "d":"http://cdn.dribbble.com/assets/favicon-63b2904a073c89b52b19aa08cebc16a154bcf83fee8ecc6439968b1e6db569c7.ico",
-    "m":"http://developer.mozilla.org/static/img/favicon32.7f3da72dcea1.png",
-    "g":"http://github.com/favicon.ico",
-    "j":"http://jquery.com/favicon.ico",
-    "n":"http://nodejs.org/static/images/favicons/favicon.ico",
-    "v":"http://cn.vuejs.org/images/icons/favicon-32x32.png",
-    "r":"https://zh-hans.reactjs.org/favicon.ico",
-    "s":"http://www.mongodb.com/assets/images/global/favicon.ico",
-    "z":"https://www.colorzilla.com/favicon.ico",
-    "p":"https://compressjpeg.com/images/compressjpeg/icon.png",
-    "h":"https://wallhaven.cc/favicon.ico",
-    "x":"https://www.cssmatic.com/img/favicon.png"
+    "w": "http://wangdoc.com/assets/icons/favicon-32x32.png",
+    "t": "http://translate.google.cn/favicon.ico",
+    "b": "http://bitwiser.in/favicon.ico",
+    "c": "http://codropspz-tympanus.netdna-ssl.com/codrops/wp-content/themes/codropstheme03/favicons/favicon.ico",
+    "d": "http://cdn.dribbble.com/assets/favicon-63b2904a073c89b52b19aa08cebc16a154bcf83fee8ecc6439968b1e6db569c7.ico",
+    "m": "http://developer.mozilla.org/static/img/favicon32.7f3da72dcea1.png",
+    "g": "http://github.com/favicon.ico",
+    "j": "http://jquery.com/favicon.ico",
+    "n": "http://nodejs.org/static/images/favicons/favicon.ico",
+    "v": "http://cn.vuejs.org/images/icons/favicon-32x32.png",
+    "r": "https://zh-hans.reactjs.org/favicon.ico",
+    "s": "http://www.mongodb.com/assets/images/global/favicon.ico",
+    "z": "https://www.colorzilla.com/favicon.ico",
+    "p": "https://compressjpeg.com/images/compressjpeg/icon.png",
+    "h": "https://wallhaven.cc/favicon.ico",
+    "x": "https://www.cssmatic.com/img/favicon.png"
 };
 
 //去出localStorage中的websites对应的sites
 let hashInLocalStorage = JSON.parse(localStorage.getItem("websites") || "null");
-if (hashInLocalStorage){
+if (hashInLocalStorage) {
     sites = hashInLocalStorage;
 }
 
 
 let kbn = document.getElementById("keyBoardNav");
 
-for (let i=0;i<keys.length;i++) {
+for (let i = 0; i < keys.length; i++) {
     let div = document.createElement("div");
     kbn.appendChild(div);
-    for (let j = 0;j<keys[i].length;j++){
+    for (let j = 0; j < keys[i].length; j++) {
         let kbd = document.createElement("kbd");
         let btn = document.createElement("button");
         let span = document.createElement("span");
@@ -66,7 +66,7 @@ for (let i=0;i<keys.length;i++) {
             let img = document.createElement("img");
             img.src = siteImgs[keys[i][j]];
             kbd.appendChild(img);
-        }else if(!siteImgs[keys[i][j]]&&sites[keys[i][j]]){
+        } else if (!siteImgs[keys[i][j]] && sites[keys[i][j]]) {
             let img = document.createElement("img");
             img.src = "images/undefined.png";
             img.alt = "icon";
@@ -76,25 +76,26 @@ for (let i=0;i<keys.length;i++) {
 
 
         //点击按钮后添加 网址、图片
-        btn.onclick = function(){
+        btn.onclick = function () {
             let key = this.parentNode.id;
-            let temp = prompt("输入一个新网址或者clear所有缓存地址,原本的地址为:"+sites[keys[i][j]]);
+            let temp = prompt("输入一个新网址或者clear所有缓存地址,原本的地址为:" + sites[keys[i][j]]);
             let img = null;
             // console.log(temp);
-            if(temp !== null) {
+            if (temp !== null) {
                 // if()
                 console.log(this.parentElement.lastChild);
                 img = document.createElement("img");
                 img.src = "images/undefined.png";
             }
-            if (temp === "clear"){
+            if (temp === "clear") {
                 localStorage.clear();
                 window.location.reload();
-            }else if (temp){
+            } else if (temp) {
                 sites[key] = temp;
-                localStorage.setItem("websites",JSON.stringify(sites));
+                localStorage.setItem("websites", JSON.stringify(sites));
                 kbd.appendChild(img);
             }
+            stopBubble();
         };
         div.appendChild(kbd);
         kbd.appendChild(btn);
@@ -102,10 +103,24 @@ for (let i=0;i<keys.length;i++) {
     }
 }
 
-document.onkeypress = function(e){
+document.onkeypress = function (e) {
     let key = e.key;
     let website = sites[key];
-    if (sites[key]){
-        window.open("http://"+website, "_blank");
+    if (sites[key]) {
+        window.open("http://" + website, "_blank");
     }
 };
+
+document.addEventListener("click",function(e){
+    let key = e.target.offsetParent.id;
+    let website = sites[key];
+    if (sites[key]) {
+        window.open("http://" + website, "_blank");
+    }
+});
+function stopBubble(e) {
+    if ( e && e.stopPropagation )
+        e.stopPropagation();
+    else
+        window.event.cancelBubble = true;
+}
