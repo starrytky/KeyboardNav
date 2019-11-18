82c0b8a785a7b4d247e16ad025cee5cb2089fb01
starrytky <starrytky@qq.com>
2019-11-18 14:47:11 +0800

    代码整理

--- js/index.js
+++ js/index.js
@@ -1,107 +1,15 @@
+//1.初始化数据
 let hash = init();
 let keys = hash.keys;
 let sites = hash.sites;
-let siteImgs = hash.siteImgs;
+let siteImages = hash.siteImages;
+
 
 
 //2，生成键盘
-let kbn = document.getElementById("keyBoardNav");
-
-for (let i = 0; i < keys.length; i++) {
-    let div = c("div");
-    kbn.appendChild(div);
-    for (let j = 0; j < keys[i].length; j++) {
-        let kbd = c("kbd");
-        let btn = c("button");
-        let span = c("span");
-
-        //添加文字，图片
-        let img = c("img");
-        span.textContent = keys[i][j];
-        kbd.id = keys[i][j];
-        if (siteImgs[keys[i][j]]) {
-            img.src = siteImgs[keys[i][j]];
-            kbd.appendChild(img);
-        } else if (!siteImgs[keys[i][j]] && sites[keys[i][j]]) {
-            img.src = "images/undefined.png";
-            img.alt = "icon";
-            kbd.appendChild(img);
-        }
-        img.onerror = function (e) {
-            e.target.src = "images/undefined.png";
-        };
-        btn.textContent = "编辑";
+generateKeyboard(keys,sites,siteImages);
 
 
-        //点击按钮后添加 网址、图片
-        btn.onclick = function () {
-            let key = this.parentNode.id;
-            let temp = prompt("输入一个新网址或者clear所有缓存地址,原本的地址为:" + sites[keys[i][j]]);
-            let img = null;
-            // console.log(temp);
-            if (temp !== null) {
-                console.log(this.parentElement.lastChild);
-                img = c("img");
-                img.src = "images/undefined.png";
-            }
-            if (temp === "clear") {
-                localStorage.clear();
-                window.location.reload();
-            } else if (temp) {
-                sites[key] = temp;
-                localStorage.setItem("websites", JSON.stringify(sites));
-                kbd.appendChild(img);
-            }
-            stopBubble();
-        };
-        div.appendChild(kbd);
-        kbd.appendChild(btn);
-        kbd.appendChild(span);
-    }
-}
-
-//3.监听键盘
-document.onkeypress = function (e) {
-    let key = e.key;
-    let website = sites[key];
-    if (sites[key]) {
-        window.open("https://" + website, "_blank");
-    }
-};
-
-//4.监听点击
-let kbds = document.getElementById("keyBoardNav");
-document.addEventListener("click", function (e) {
-    let key = e.target.id;
-    if (sites[key]) {
-        window.open("https://" + sites[key], "_blank");
-    }
-});
-//
-// let kbds = document.getElementById("keyBoardNav");
-// kbds.onclick = function (e) {
-//     let key = e.target.id;
-//     let website = sites[key];
-//     if (sites[key]){
-//         window.open("http://" + website, "_blank");
-//     }
-// };
-
-function getFromLocalStorage(e){
-    return JSON.parse(localStorage.getItem(e) || "null");
-}
-
-function c(tagName){
-    return document.createElement(tagName);
-}
-
-//阻止事件冒泡
-function stopBubble(e) {
-    if (e && e.stopPropagation)
-        e.stopPropagation();
-    else
-        window.event.cancelBubble = true;
-}
 function init(){
     //初始化
     let keys = [
@@ -125,10 +33,10 @@ function init(){
         "s": "www.mongodb.com/cn",
         "z": "www.colorzilla.com",
         "p": "compressjpeg.com",
-        "h": "https://wallhaven.cc/",
-        "x": "https://www.cssmatic.com/box-shadow"
+        "h": "wallhaven.cc/",
+        "x": "www.cssmatic.com/box-shadow"
     };
-    let siteImgs = {
+    let siteImages = {
         "w": "http://wangdoc.com/assets/icons/favicon-32x32.png",
         "t": "http://translate.google.cn/favicon.ico",
         "b": "http://bitwiser.in/favicon.ico",
@@ -156,6 +64,114 @@ function init(){
     return {
         "keys":keys,
         "sites":sites,
-        "siteImgs":siteImgs
+        "siteImages":siteImages
     };
+}
+
+//2.创建键盘
+function generateKeyboard(keys, sites, siteImages){
+    let kbn = document.getElementById("keyBoardNav");
+
+    for (let i = 0; i < keys.length; i++) {
+        let div = c("div");
+        kbn.appendChild(div);
+        for (let j = 0; j < keys[i].length; j++) {
+            let kbd = c("kbd");
+            kbd.id = keys[i][j];
+
+            let span = c("span");
+            span.textContent = keys[i][j];
+            //添加文字，图片
+            let img = c("img");
+            if (siteImages[keys[i][j]]) {
+                img.src = siteImages[keys[i][j]];
+                kbd.appendChild(img);
+            } else if (!siteImages[keys[i][j]] && sites[keys[i][j]]) {
+                img.src = "images/undefined.png";
+                img.alt = "icon";
+                kbd.appendChild(img);
+            }
+            img.onerror = function (e) {
+                e.target.src = "images/undefined.png";
+            };
+
+            let btn = c("button");
+            btn.textContent = "编辑";
+            btn.addEventListener("click", function(){
+                let key = this.parentNode.id;
+                let temp = prompt("输入一个新网址或者clear所有缓存地址,原本的地址为:" + sites[keys[i][j]]);
+                let img = null;
+                if (this.parentElement.firstChild.tagName!=="IMG") {
+                    img = c("img");
+                    img.src = "images/undefined.png";
+                    kbd.appendChild(img);
+                }
+                if (temp === "clear") {
+                    localStorage.clear();
+                    window.location.reload();
+                } else if (temp) {
+                    sites[key] = temp;
+                    localStorage.setItem("websites", JSON.stringify(sites));
+
+                }
+                stopBubble();
+            });
+
+
+            kbd.appendChild(btn);
+            kbd.appendChild(span);
+            div.appendChild(kbd);
+        }
+    }
+}
+//3.监听用户动作
+listerToUser();
+//监听用户动作
+function listerToUser(){
+    //3.监听键盘
+    document.onkeypress = function (e) {
+        let key = e.key;
+        let website = sites[key];
+        if (sites[key]) {
+            window.open("https://" + website, "_blank");
+        }
+    };
+
+//4.监听点击
+    let kbds = document.getElementById("keyBoardNav");
+    kbds.addEventListener("click", function (e) {
+        let key = e.target.id;
+        if (sites[key]) {
+            window.open("https://" + sites[key], "_blank");
+        }
+    });
+    //
+// let kbds = document.getElementById("keyBoardNav");
+// kbds.onclick = function (e) {
+//     let key = e.target.id;
+//     let website = sites[key];
+//     if (sites[key]){
+//         window.open("http://" + website, "_blank");
+//     }
+// };
+
+}
+
+
+
+//工具函数
+function getFromLocalStorage(e){
+    return JSON.parse(localStorage.getItem(e) || "null");
+}
+
+function c(tagName){
+    return document.createElement(tagName);
+}
+
+//阻止事件冒泡
+function stopBubble(e) {
+    if (e && e.stopPropagation)
+        e.stopPropagation();
+    else
+        window.event.cancelBubble = true;
 }
